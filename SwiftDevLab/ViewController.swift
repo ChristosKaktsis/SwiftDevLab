71e5b2e86adb84b87fda8e002599bb08b4549919
Chris Kaktsis <christoskaktsis@gmail.com>
2024-06-20 13:20:10 +0300

    Initial Commit

---
+++ SwiftDevLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDevLab
+//
+//  Created by Christos Kaktsis on 20/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftDevLabUITests/SwiftDevLabUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDevLabUITestsLaunchTests.swift
+//  SwiftDevLabUITests
+//
+//  Created by Christos Kaktsis on 20/6/24.
+//
+
+import XCTest
+
+final class SwiftDevLabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
